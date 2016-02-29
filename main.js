46e95b6ba7c1681509f504a28629424484e232dc
pimgeek <wudibiz@163.com>
2016-02-29 10:04:40 +0800

    把 flow 工具迁移到 hintsnet 项目后，完成 bug 修复

--- main.js
+++ main.js
@@ -1,28 +1,22 @@
-// 测试 hintsnet 引思网库函数是否工作正常
+var inputPort = document.querySelector('#input');
+var outputPort = document.querySelector('#output');
+var inputStream = Rx.Observable.fromEvent(inputPort, 'input');
 
-var hintPipe1 = {
-  "inPort": "a",
-  "outPort": "b"
-};
-var hintPipe2 = {
-  "inPort": "a",
-  "outPort": "c"
-};
-var hintPipe3 = {
-  "inPort": "a",
-  "outPort": "b"
-};
-
-var hintPipesStr = "ab中国人|->|cd困了、累了、喝红牛 #ads #biz-pitch\n" +
-  "偏序图渲染服务|->|以图答问微信号 #partial-order #hintsnet-meta #biz-pitch \n" +
-  "引思网已部署完毕|->|讨论共识可作为项目完成度的单元测试工具 #partial-order #hintsnet-meta #biz-pitch \n";
-
-var hintPipesJSONArray = hintPipesStr2JSONArray(hintPipesStr);
-console.log(hintPipesJSONArray);
-
-var dotEdgesStr = hintPipesJSONArray2DotEdgesStr(hintPipesJSONArray);
-console.log(dotEdgesStr);
-
-//console.log(hintPipeIsEqual(hintPipe1, hintPipe2));
-//console.log(hintPipeIsEqual(hintPipe1, hintPipe3));
-//console.log(hintPipeIsEqual(hintPipe2, hintPipe3));
+function loadProcessor(processor) {
+  var readInputStream = inputStream.startWith('1')
+    .map(function() {
+      return inputPort.value;
+    });
+  var outputStream = readInputStream
+    .map(processor);
+  var subscription = outputStream.subscribe(
+    function(val) {
+      outputPort.value = val;
+    },
+    function(err) {
+      console.log('出错啦: ' + err);
+    },
+    function() {
+      console.log('完成啦。');
+    });
+}
