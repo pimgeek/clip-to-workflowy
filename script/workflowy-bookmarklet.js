66e7f706d6a79e2d4ff4672a1297804c448ab0d8
pimgeek <wudibiz@163.com>
2016-03-18 14:55:43 +0800

    重构 JS 代码，并且修改 opml 的输出结构

--- script/workflowy-bookmarklet.js
+++ script/workflowy-bookmarklet.js
@@ -6,25 +6,33 @@ javascript: (function() {
     return (o);
   }
 
-  var bullet = eopml(document.title);
-  var link = eopml(location.href) + ' ';
-  var selected = eopml(window.getSelection().toString());
+  var docTitle = eopml(document.title);
+  var docUrl = eopml(location.href) + ' ';
+  var docSelection = eopml(window.getSelection().toString());
   var isWorkFlowy = location.href.indexOf('workflowy.com/#');
   var isChrome = !!window.chrome;
 
-  if (selected) { link = '&#10;' + link + '&#10;&#10;'; }
   if (isWorkFlowy !== -1) {
-    var bullet = '&lt;i&gt;See: &quot;' +
-      bullet.replace(/ - WorkFlowy$/, '') +
+    var docTitle = '&lt;i&gt;See: &quot;' +
+      docTitle.replace(/ - WorkFlowy$/, '') +
       '&quot;&lt;/i&gt;';
   }
 
-  var clip = '<opml><body><outline text="' +
-    bullet +
-    '" _note="' +
-    selected +
-    link +
-    '" /></body></opml>';
+  var clip = '<?xml version="1.0"?>' +
+    '<opml version="2.0">' +
+    '  <head>' +
+    '    <ownerEmail>wudibiz@163.com</ownerEmail>' +
+    '  </head>' +
+    '  <body>' + 
+    '    <outline text="|-&amp;gt;| #hint-pipe #to-refine " >' +
+    '      <outline text="关键问题" >' +
+    '        <outline text="' + docSelection + '" /></outline>' +
+    '      <outline text="问题来源" >' +
+    '        <outline text="描述一下问题的来龙去脉" /></outline>' +
+    '      <outline text="相关资源" >' +
+    '        <outline text="' + docTitle + '" _note="' + docUrl +'" /></outline>' +
+    '  </body>' +
+    '</opml>';
 
   if (isChrome && clip.length > 2000) {
     window.open('' +
@@ -33,7 +41,7 @@ javascript: (function() {
       encodeURIComponent(clip) +
       '');
   } else {
-    var userInput = prompt('1\) COPY the text below. (Ctrl C\) or \(Cmd C\)\n\n2\) OK = Launch WorkFlowy Home.\n    CANCEL = Go to WorkFlowy manually.\n\n3\) PASTE into a new WorkFlowy bullet.   \(v2.4\)\n', clip);
+    var userInput = prompt('1\) COPY the text below. (Ctrl C\) or \(Cmd C\)\n\n2\) OK = Launch WorkFlowy Home.\n    CANCEL = Go to WorkFlowy manually.\n\n3\) PASTE into a new WorkFlowy docTitle.   \(v2.4\)\n', clip);
     if (userInput !== null) {
       if (isWorkFlowy !== -1) { location.href = inbox; } else { ; }
     } else {
