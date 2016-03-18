84c1af6fd0c892c9cd6593efda4bb1aa88b2394f
pimgeek <wudibiz@163.com>
2016-03-18 11:00:12 +0800

    修改 dot2gml 转换脚本，使它可以正确转换 type, fill 和 fontSize 等参数值

--- script/dot2graphml_gml.sh
+++ script/dot2graphml_gml.sh
@@ -2,12 +2,22 @@
 
 DOT_INPUT=${1};
 GML_OUTPUT="${DOT_INPUT}.gml";
-GRAPHML_GML_OUTPUT_UTF8="${2}.utf8";
-GRAPHML_GML_OUTPUT_GBK="${2}";
+TS=`date +%m%s`;
+TMP1="/tmp/dot2gml_${DOT_INPUT}_${TS}.1"
+TMP2="/tmp/dot2gml_${DOT_INPUT}_${TS}.2"
+TMP3="/tmp/dot2gml_${DOT_INPUT}_${TS}.3"
+TMP4="/tmp/dot2gml_${DOT_INPUT}_${TS}.4"
+GRAPHML_GML_OUTPUT="${2}";
 
 gv2gml ${DOT_INPUT} > ${GML_OUTPUT};
-sed 's/ name/ label/' ${GML_OUTPUT} > ${GRAPHML_GML_OUTPUT_UTF8};
-iconv -futf8 -tgbk ${GRAPHML_GML_OUTPUT_UTF8} > ${GRAPHML_GML_OUTPUT_GBK};
+sed 's/ name/ label/' ${GML_OUTPUT} > ${TMP1};
+sed -r 's/fontSize "([0-9]+)px"/fontSize \1/' ${TMP1} > ${TMP2};
+sed -r 's/type "[^"]+"/type "rectangle"/' ${TMP2} > ${TMP3};
+sed -r 's/fill "[^"]+"/fill "#FFFF99"/' ${TMP3} > ${TMP4};
+iconv -f"UTF8" -t"GBK" ${TMP4} > ${GRAPHML_GML_OUTPUT};
 
 rm -f ${GML_OUTPUT};
-rm -f ${GRAPHML_GML_OUTPUT_UTF8};
+rm -f ${TMP1};
+rm -f ${TMP2};
+rm -f ${TMP3};
+rm -f ${TMP4};
